3511828e5feb64ef5f00f52c2c3150f0d586c28c
pruthviraj <marganepruthviraj@gmail.com>
2025-06-19 21:46:30 +0530

    build fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -15,16 +15,6 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@repo/db/client', 'prisma'],
-  },
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
